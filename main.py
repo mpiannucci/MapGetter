28511e9158be65b35ca35640035077b478da36ad
Matthew Iannucci <mpiannucci@gmail.com>
2014-03-17 17:37:11 -0400

    Will now get a map with the correct url. No interface yet.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import web
+import ConfigParser
 
 # Map out the URLs
 urls = (
     '/', 'Index',
+    '/mapc(.+)', 'MapCenter', 
+    '/mapl(.+)', 'MapLatLon'
 )
 
 # Set the debugging messages
-web.config.debug = False
+web.config.debug = True
 
 ### Define the globals
 t_globals = {
@@ -17,6 +20,10 @@ render = web.template.render('templates', base='base', globals=t_globals)
 # Create the web application
 app = web.application(urls, globals())
 
+config = ConfigParser.RawConfigParser()
+config.read('key.cfg')
+StaticAPIKey = config.get('api-keys', 'mapskey')
+
 ##------------------------------------------------------------------------
 ## Web Page Class Definitions
 
@@ -25,16 +32,51 @@ class Index:
     def GET(self):
         return render.index()
 
+class MapCenter:
+    '''Show the static map image around a center'''
+    def GET(self, options):
+        # Options: 0=Address, 1=City, 2=State, 3=zoom, 4=typeofmap(satellite), 5=format(png32)
+        # Options should be comma delimited
+        opts = options.split(',')
+        baseURL = 'http://maps.googleapis.com/maps/api/staticmap?'
+        center = 'center=' + opts[0] + ',' + opts[1] + ',' + opts[2]
+        center.replace(' ', '+')
+        zoom = '&zoom=' + str(opts[3])
+        mtype = '&maptype=' + opts[4]
+        formt = '&format=' + opts[5]
+        size = '&size=640x640'    # Biggest size for non paid accounts
+        sensor = "&sensor=false"  # We will never us the users location
+        apikey = '&key=' + StaticAPIKey
+        url = baseURL + center + zoom + size + mtype + formt + sensor + apikey
+        return render.map(url, opts)
+
+class MapLatLon:
+    '''Show the static map image around a lat,lon pair'''
+    def GET(self, options):
+        # Options: 0=lat, 1=lon, 2=zoom, 3=typeofmap(satellite), 4=format(png32)
+        # Options should be comma delimited
+        opts = options.split(',')
+        baseURL = 'http://maps.googleapis.com/maps/api/staticmap?'
+        center = 'center=' + str(opts[0]) +',' + str(opts[1])
+        zoom = '&zoom=' + str(opts[2])
+        mtype = '&maptype=' + str(opts[3])
+        formt = '&format=' + str(opts[4])
+        size = '&size=640x640'    # Biggest size for non paid accounts
+        sensor = "&sensor=false"  # We will never us the users location
+        apikey = '&key=' + StaticAPIKey
+        url = baseURL + center + zoom + size + mtype + formt + sensor + apikey
+        return render.map(url, opts)
+
 def notfound():
-    """ Create the not found page """
-    return web.notfound("Sorry, the page you were looking for was not found.")
+    ''' Create the not found page '''
+    return web.notfound('Sorry, the page you were looking for was not found.')
     # You can use template result like below, either is ok:
     #return web.notfound(render.notfound())
     #return web.notfound(str(render.notfound()))
 
 def internalerror():
-    """ Create the internal error page """
-    return web.internalerror("The server says: No soup for you!")
+    ''' Create the internal error page '''
+    return web.internalerror('The server says: No soup for you!')
 
 ### Create the not found app
 app.notfound = notfound
--- templates/__init__.py
+++ templates/__init__.py
@@ -15,7 +15,9 @@ def base (page):
     extend_([u'    <link rel="stylesheet" type="text/css" href="/static/Styles/screen.css" media="only screen and (min-device-width: 768px)" />\n'])
     extend_([u'</head>\n'])
     extend_([u'<body>\n'])
-    extend_([u'    ', escape_(page, False), u'\n'])
+    extend_([u'    <div id="mapImage">\n'])
+    extend_([u'        ', escape_(page, False), u'\n'])
+    extend_([u'    </div>\n'])
     extend_([u'</body>\n'])
     extend_([u'</html>\n'])
 
@@ -29,10 +31,29 @@ def index():
     __lineoffset__ = -5
     loop = ForLoop()
     self = TemplateResult(); extend_ = self.extend
-    extend_([u'<h1>HelloWorld</h1>\n'])
+    extend_([u'\n'])
 
     return self
 
 index = CompiledTemplate(index, 'templates/index.html')
 join_ = index._join; escape_ = index._escape
+
+# coding: utf-8
+def map (url, opts):
+    __lineoffset__ = -4
+    loop = ForLoop()
+    self = TemplateResult(); extend_ = self.extend
+    extend_([u'\n'])
+    if len(opts) > 5:
+        opts[0] = opts[0].replace('+', ' ')
+        extend_([u'<h2>Map of ', escape_(opts[0], True), u', ', escape_(opts[1], True), u', ', escape_(opts[2], True), u'</h2>\n'])
+        extend_([u'<img src="', escape_(url, True), u'" />\n'])
+    else:
+        extend_([u'<h2>Map of ', escape_(opts[0], True), u',', escape_(opts[1], True), u'</h2>\n'])
+        extend_([u'<img src="', escape_(url, True), u'" />\n'])
+
+    return self
+
+map = CompiledTemplate(map, 'templates/map.html')
+join_ = map._join; escape_ = map._escape
 
