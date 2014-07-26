2ab5265ea8ad3881a19088787d708e841e34a459
Matthew Iannucci <mpiannucci@gmail.com>
2014-07-25 23:21:44 -0400

    removed oopsie

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ zoomvalues = []
 for i in range(5, 20):
     tup = (str(i), str(i))
     zoomvalues.append(tup)
-
 map_form = web.form.Form(web.form.Checkbox('By Coordinates', checked=True, id="coordcheck", onClick='handleCheck(this);'),
                          web.form.Textbox('Address', id='addressbox', disabled=False),
                          web.form.Textbox('City', id='citybox', disabled=False),
