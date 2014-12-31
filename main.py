91676ef072a3de0a254841c73429f03925c207e9
Matthew Iannucci <mpiannucci@gmail.com>
2014-12-30 21:48:12 -0500

    Removed form from python into raw html

--- main.py
+++ main.py
@@ -17,29 +17,12 @@ render = web.template.render('templates', base='base', globals=t_globals)
 app = web.application(urls, globals())
 
 # ------------------------------------------------------------------------
-# Form Definitions
-# 
-zoomvalues = []
-for i in range(5, 20):
-    tup = (str(i), str(i))
-    zoomvalues.append(tup)
-map_form = web.form.Form(web.form.Checkbox('By Coordinates', checked=True, id="coordcheck", onClick='handleCheck(this);'),
-                         web.form.Textbox('Address', id='addressbox', disabled=False),
-                         web.form.Textbox('City', id='citybox', disabled=False),
-                         web.form.Textbox('State', id='statebox', disabled=False),
-                         web.form.Textbox('Latitude', id='latbox'),
-                         web.form.Textbox('Longitude', id='lonbox'),
-                         web.form.Dropdown('Zoom', zoomvalues, id='zoomdrop'),
-                         web.form.Button('Get My Map', id='formbutton', type='button', onClick='handleGetMap()'))
-
-# ------------------------------------------------------------------------
 # Web Page Class Definitions
 
 class Index:
     '''Main index page'''
     def GET(self):
-        form = map_form()
-        return render.index(form)
+        return render.index()
 
 def notfound():
     ''' Create the not found page '''
